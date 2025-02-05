61f66cfb1067582668fd4c11942004d0e519cb46
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2025-02-05 23:07:53 +0200

    Add files via upload

---
+++ tpz_objectloader/config.lua
@@ -0,0 +1,31 @@
+Config = {}
+
+---------------------------------------------------------------
+--[[ General Settings ]]--
+---------------------------------------------------------------
+
+-- The wait time for checking nearby objects.
+Config.WaitTime = 2000 -- Time in milliseconds.
+
+---------------------------------------------------------------
+--[[ Object Locations ]]--
+---------------------------------------------------------------
+
+Config.Locations = {
+
+    -- Example
+    { 
+        Object = 'p_fence02cx',
+
+        Coords = { 
+            x = -1674.8743896484375, y = -339.5870666503906, z = 172.87277221679688, 
+            pitch = -6.19514608383178, roll = 4.9673957824707, yaw = 123.85027313232422 
+        },
+
+        PlaceObjectOnGroundProperly = false,
+
+        ObjectRenderDistance = 30,
+    },
+
+}
+
